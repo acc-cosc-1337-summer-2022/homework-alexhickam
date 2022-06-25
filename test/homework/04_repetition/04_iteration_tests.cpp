80721a348dde28c4a67fe543d7031587ad22e3d6
alexhickam <alex.hickam@g.austincc.edu>
2022-06-25 14:15:15 -0500

    tests working

--- test/homework/04_repetition/04_iteration_tests.cpp
+++ test/homework/04_repetition/04_iteration_tests.cpp
@@ -1,7 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "dna.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+TEST_CASE("Verify reutrn double for how many C and G in dna ") {
+	
+	REQUIRE(get_gc_content("AGCTATAG") ==.375);
+	REQUIRE(get_gc_content("CGCTATAG") ==.50);
+}
 
+TEST_CASE("Verify string is reversed")
+{
+	
+	REQUIRE(get_reverse_string("AGCTATAG") =="GATATCGA");
+	REQUIRE(get_reverse_string("CGCTATAG") =="GATATCGC");
+}
+
+TEST_CASE("Verify string compliment")
+{
+	
+	REQUIRE(get_dna_complement("AAAACCCGGT") =="ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") =="ATTTTCCGGG");
+}
