162bfc8b1617447787904b6adff4ed3ed58d55fa
alexhickam <alex.hickam@g.austincc.edu>
2022-07-01 18:28:30 -0500

    homework 5

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,30 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 
-int main() 
-{
+int main() {
+  TicTacToe game;
+  string first_player{""};
+  char user_choice{'Y'};
 
-	return 0;
+  do {
+    cout << "Enter first player: ";
+    cin >> first_player;
+    game.start_game(first_player);
+    int position{0};
+    while (!game.game_over()) {
+      cout << "Enter a position: ";
+      cin >> position;
+      game.mark_board(position);
+      game.display_board();
+    }
+    cout << "Game over!\nDo you want to play again? (y/n): ";
+    cin >> user_choice;
+  } while (user_choice == 'y' || user_choice == 'Y');
+
+  return 0;
 }
