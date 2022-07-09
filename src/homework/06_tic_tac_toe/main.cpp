497fe87203cb31866ad699f668df6d9a5dd50a20
alexhickam <alex.hickam@g.austincc.edu>
2022-07-09 18:19:06 -0500

    HomeWork 6 Complete

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,30 +1,64 @@
 #include "tic_tac_toe.h"
-#include <iostream>
-#include <string>
-using std::cin;
+#include<iostream>
+#include<string>
+
 using std::cout;
-using std::endl;
+using std::cin;
 using std::string;
 
-int main() {
-  TicTacToe game;
-  string first_player{""};
-  char user_choice{'Y'};
+int main() 
+{
+	TicTacToe game;
+	string first_player;
+	char user_choice = 'y';
 
-  do {
-    cout << "Enter first player: ";
-    cin >> first_player;
-    game.start_game(first_player);
-    int position{0};
-    while (!game.game_over()) {
-      cout << "Enter a position: ";
-      cin >> position;
-      game.mark_board(position);
-      game.display_board();
-    }
-    cout << "Game over!\nDo you want to play again? (y/n): ";
-    cin >> user_choice;
-  } while (user_choice == 'y' || user_choice == 'Y');
+	do
+	{
+		cout<<"Enter first player X or O: ";
+		cin>>first_player;
+		while(first_player != "X" && first_player != "O")
+		{
+			cout<<"\nInvalid Entry! Enter first player X or O: ";
+			cin>>first_player;
+		}
 
-  return 0;
+		game.start_game(first_player);
+
+		int position;
+
+		while(!game.game_over())
+		{
+			cout<<"Enter a position: ";
+			cin>>position;
+			while(position < 1 || position > 9)
+			{
+				cout<<"Invalid Position! Choose a position between 0-9: ";
+				cin>>position; 
+			}
+			
+			game.mark_board(position);
+			game.display_board();
+			
+		}
+		if( game.get_winner() == "X" || game.get_winner() == "O")
+			{
+			cout<<game.get_winner() <<" WINS!!! "<<"\n" ;
+			}
+		else
+		{
+			cout<<"Draw! No Winner!\n";
+		}
+		
+		
+		cout<<"Play again? Enter Y or N: ";
+		cin>>user_choice;
+		while(user_choice != 'Y' && user_choice != 'y' && user_choice != 'N' && user_choice != 'n')
+        {
+            cout<<"Invalid choice! Type 'Y' to play again or 'N' to end program: ";
+            cin>>user_choice;
+        }
+		
+	} while (user_choice == 'y' || user_choice == 'Y');
+	
+	return 0;
 }
--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -1,35 +1,151 @@
-#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
-                          // in one cpp file
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
-  REQUIRE(true == true);
+	REQUIRE(true == true);
 }
 
-TEST_CASE("Verify game_over functions", "game_over") {
-  TicTacToe game;
-  REQUIRE(game.game_over() == false);
+TEST_CASE("Test if the game is a tie, verify the winner is C")
+{ 
+	TicTacToe game;
+	game.mark_board(1);                    
+	REQUIRE(game.game_over() == false);
+	game.mark_board(2);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(3);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(4);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(5);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(7);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(6);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(9);
+	REQUIRE(game.game_over() == false);
+	game.mark_board(8);
+	REQUIRE(game.game_over() == true);
+
+	REQUIRE(game.get_winner() == "C");
 }
 
-TEST_CASE("Test if the board is full") {
-  TicTacToe game;
-  game.mark_board(1);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(2);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(3);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(4);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(5);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(6);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(7);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(9);
-  REQUIRE(game.game_over() == false);
-  game.mark_board(8);
-  REQUIRE(game.game_over() == true);
+TEST_CASE("Test first player set to X")
+{
+	TicTacToe game;
+	game.start_game("X");
+	REQUIRE(game.get_player() == "X");
+}
+
+TEST_CASE("Test first player set to O")
+{
+	TicTacToe game;
+	game.start_game("O");
+	REQUIRE(game.get_player() == "O");
+}
+
+TEST_CASE("Test win by first column")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(1); //X    X|O|O
+	game.mark_board(2); //O    X| | 
+	game.mark_board(4); //X    X| | 
+	game.mark_board(3); //O
+	game.mark_board(7); //X
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win by second column")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(2); //X    O|X|O
+	game.mark_board(1); //O     |X|
+	game.mark_board(5); //X     |X|
+	game.mark_board(3); //O
+	game.mark_board(8); //X
+	
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win by third column")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(3); //X    O|O|X
+	game.mark_board(1); //O     | |X
+	game.mark_board(6); //X     | |X
+	game.mark_board(2); //O
+	game.mark_board(9); //X
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win by first row")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(1); //X    X|X|X
+	game.mark_board(4); //O    O|O|
+	game.mark_board(2); //X     | | 
+	game.mark_board(5); //O
+	game.mark_board(3); //X
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win by second row")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(4); //X    O|O| 
+	game.mark_board(1); //O    X|X|X 
+	game.mark_board(5); //X     | |
+	game.mark_board(2); //O
+	game.mark_board(6); //X
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win by third row")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(7); //X
+	game.mark_board(1); //O    O|O|
+	game.mark_board(8); //X     | |
+	game.mark_board(2); //O    X|X|X
+	game.mark_board(9); //X
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win diagonally from top left")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(1); //X    X|O|O
+	game.mark_board(2); //O     |X|
+	game.mark_board(5); //X     | |X
+	game.mark_board(3); //O
+	game.mark_board(9); //X
+
+	REQUIRE(game.game_over() == true);
+}
+
+TEST_CASE("Test win diagonally from bottom left left")
+{
+	TicTacToe game;
+	game.start_game("X");
+	game.mark_board(7); //X     |O|X
+	game.mark_board(2); //O     |X|O
+	game.mark_board(5); //X    X| |
+	game.mark_board(4); //O     
+	game.mark_board(3); //X     
+
+	REQUIRE(game.game_over() == true);
 }
